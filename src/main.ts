5f453f8219f714a011a874ec68d88b7b65aede74
Tuxtasinboyev Omadbek <tuxtasinboyev@kali.kali>
2025-09-02 10:00:43 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   app.enableCors(
     {
       origin: '*',
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
       allowedHeaders: ['Content-Type', 'Authorization']
     }
   );
